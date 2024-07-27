97cbade637cc09a3c6b9d11891ca1fa46f154cbe
thurssdar <145630790+thurssdar@users.noreply.github.com>
2024-07-28 03:39:43 +0700

    controller,model,database

---
+++ uasdara/database/migrations/2024_07_27_194306_create_mahasiswa_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim')->unique();
+            $table->string('nama');
+            $table->string('tempat');
+            $table->date('tanggal');
+            $table->string('alamat');
+            $table->string('jk');
+            $table->string('agama');
+            // $table->bigInteger('semester_id');
+            // $table->bigInteger('tahunajar_id');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
--- uasdara/routes/web.php
+++ uasdara/routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-
+use App\Http\Controllers\PenjadwalanController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\TahunAjarController;
+use App\Http\Controllers\MataKuliahController;
+use App\Http\Controllers\SemesterController;
+use App\Http\Controllers\DosenController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +27,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+Route::get('penjadwalan', [PenjadwalanController::class, 'index']);
+Route::get('matakuliah', [PenjadwalanController::class, 'index']);
+Route::get('semester', [PenjadwalanController::class, 'index']);
+Route::get('tahunajar', [PenjadwalanController::class, 'index']);
+Route::get('dosen', [PenjadwalanController::class, 'index']);
+Route::get('mahasiswa', [PenjadwalanController::class, 'index']);
